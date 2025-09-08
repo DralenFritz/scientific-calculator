97f2bfca6d3e757b5a9edb5d9365e14a9ed3366c
Dralen <112644081+DralenFritz@users.noreply.github.com>
2025-09-08 18:38:47 +0000

    implemented a few scientific functions

--- src/main.js
+++ src/main.js
@@ -9,7 +9,16 @@ function evaluateResult() {
     const convertedValue = currentValue
         .replaceAll('×','*')
         .replaceAll('÷','/')
-        .replaceAll('%', '*0.01');
+        .replaceAll('%', '*0.01')
+        .replaceAll('sin','Math.sin')
+        .replaceAll('cos','Math.cos')
+        .replaceAll('ln','Math.log')
+        .replaceAll('π','Math.PI')
+        .replaceAll('log', 'Math.log10')
+        .replaceAll('e', 'Math.E')
+        .replaceAll('tan', 'Math.tan')
+        .replaceAll('√','Math.sqrt');
+
     const result = eval(convertedValue);
     currentValue = result.toString();
     display.value = currentValue;
@@ -19,17 +28,25 @@ for (const button of buttons) {
     button.addEventListener("click", () => {
         const value = button.innerHTML;
 
-        if (value === "AC") {
+        try {
+            if (value === "AC") {
             currentValue = "";
             display.value = currentValue;
-        } else if (value === "=") {
-            currentValue.replaceAll('÷','/');
-            currentValue.replaceAll('×','*');
-            evaluateResult();
-        } else {
-            currentValue += value;
+            } else if (value === "=") {
+                currentValue.replaceAll('÷','/');
+                currentValue.replaceAll('×','*');
+                evaluateResult();
+            } else {
+                currentValue += value;
+                display.value = currentValue;
+            }
+        } catch(err) {
+            console.error(err);
+            currentValue = "ERROR";
             display.value = currentValue;
         }
+
+        
 
     })
 }
